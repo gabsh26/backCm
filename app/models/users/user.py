4cec7c118cc34a12cceb99b7c3f626c8a51c8fb2
LMGSLeslie <lesmagasa@hotmail.com>
2019-05-06 13:06:01 -0500

    Conección local bd

--- app/models/users/user.py
+++ app/models/users/user.py
@@ -4,8 +4,8 @@ from app.models.users.errors import UserAlreadyRegistered
 class User:
     @classmethod
     def register(cls, data):
-        # user = Database.select("""SELECT id FROM user WHERE username = %s""", (data['username'],))
-        # if len(user) > 0:
-        #     raise UserAlreadyRegistered("User already registered")
-        # Database.modify("""INSERT INTO user (username, password) VALUES (%, %)""", (data['username'], data['password']))
+        user = Database.select("""SELECT idUser FROM user WHERE username = %s""", (data['username'],))
+        if len(user) > 0:
+            raise UserAlreadyRegistered("User already registered")
+        Database.modify("""INSERT INTO user (username, password) VALUES (%s, %s)""", (data['username'], data['password']))
         return "usuario registrado"
